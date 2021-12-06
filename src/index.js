d94828693db7d3663bc493001e3d14efb5772d60
Rhys Devalckeneer <rhys.devalckeneer@student.ehb.be>
2021-12-06 12:08:25 +0100

    push

--- src/index.js
+++ src/index.js
@@ -1,15 +1,1 @@
-'use strict';
-
-window.onload = function(){
-    console.log('Window loaded!');
-    
-    getData();
-}
-
-async function getData() {
-    let response = await fetch('https://web2-groupbackend-teamcas.herokuapp.com/challenges');
-    console.log(response);
-    return await response.json();
-}
-
-
+console.log('test');
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const path = require('path');
 
 module.exports = {
-    mode: "development",
+    mode: 'none',
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: './assets/js/main.js',
-    }, 
+    }
 };
