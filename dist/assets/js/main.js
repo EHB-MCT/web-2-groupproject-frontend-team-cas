b7c8eabf2d8b6d72542ff5f28075c6af539dac4b
JoachimHamraoui <joachim.hamraoui@student.ehb.be>
2021-12-07 00:08:23 +0100

    final commit
    not everything works, sadly

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -3,54 +3,81 @@
 var __webpack_exports__ = {};
 
 
-window.onload = function() {
-    console.log('window loaded');
-    
-    function displayChallenges(){
-        getData('https://web2-groupbackend-teamcas.herokuapp.com/challenges')
-        .then(function (data) {
-            let challenge = document.getElementById('challengeList')
-            let htmlString;
+let challengesList;
+let challengesHTML = "";
 
-            //displaying all data
-            data.forEach(challenge => {
-                htmlString +=
-                    `<article id="card">
-                        <div>
-                            <h3>${challenge.name}</h3>
-                        </div>
-                        <div id="info">
-                            <p>${challenge.course}</p>
-                            <p>${challenge.points}</p>
-                            <p>${challenge.session}</p>
-                        </div>
-                    </article>`
+window.onload = function() {
+
+    console.log(challengesList)
+
+
+    const insertForm = document.getElementById("uploadForm")
+
+    async function createChallenge() {
+        let challengeId = document.getElementById('id').value;
+        let challengeName = document.getElementById('name').value;
+        let challengePts = document.getElementById('points').value;
+        let challengeCourse = document.getElementById('course').value;    
+        let challengeSession = document.getElementById('session').value;
+
+        fetch(`https://web2-groupbackend-teamcas.herokuapp.com/saveChallenge`, {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    id: challengeId,
+                    name: challengeName,
+                    points: challengePts,
+                    course: challengeCourse,
+                    session: challengeSession
+                })
+
+            }).then(response => {
+                return response.json()
+            }).then(data => {
+                console.log('Success:', data);
             })
-            challenge.innerHTML = htmlString
-        });
     }
 
-    displayChallenges();
+    insertForm.addEventListener('submit', event => {
+        event.preventDefault(); 
+
+        createChallenge();
+        
 
 
-
-
-
-
-
-    let uploadForm = document.getElementById('uploadForm');
-
-    uploadForm.addEventListener('submit', event => {
-        event.preventDefault();
-
-        console.log('clicked')
     });
-}
 
-async function getData(url){
-    let response = await fetch(url);
-    return await response.json();
-}
+    async function renderChallenges(){
+        console.log('Loaded my guy');
 
+        await fetch('https://web2-groupbackend-teamcas.herokuapp.com/challenges').then(response => {
+        return response.json();
+    }).then(data => {
+        challengesList = data;
+        console.log("Fetch this: ", data);
+    })
+
+        challengesList.forEach(challenge => {
+            challengesHTML += `<article id="card">
+            <div>
+                <h3>${challenge.name}</h3>
+            </div>
+            <div id="info">
+                <p>${challenge.course}</p>
+                <p>${challenge.points}</p>
+                <p>${challenge.session}</p>
+            </div>
+        </article>`
+        })
+
+        document.getElementById("challengeList").innerHTML = challengesHTML;
+    }
+
+renderChallenges();
+
+
+}
 /******/ })()
 ;
